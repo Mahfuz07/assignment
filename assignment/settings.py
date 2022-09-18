d77c3eca297ee267df071a2f048a0fa41c03756f
Mahfuz07 <amahfuzanan@gmail.com>
2022-09-18 23:19:11 +0600

    five

--- assignment/settings.py
+++ assignment/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = 'static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
